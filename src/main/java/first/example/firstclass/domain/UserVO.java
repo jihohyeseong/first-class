d8d9c321ccca9a6a2a4159ea804da3760e15b18d
jihohyeseong <hsl4720@naver.com>
2025-10-02 13:45:19 +0900

    bug fix

--- src/main/java/first/example/firstclass/domain/UserVO.java
+++ src/main/java/first/example/firstclass/domain/UserVO.java
@@ -1,21 +1,12 @@
 package first.example.firstclass.domain;
 
-import java.io.Serializable;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Setter
 @Getter
-@ToString
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserVO implements Serializable {
+public class UserVO {
 
-	private static final long serialVersionUID = 1L;
 	private String username;
 	private String password;
 	private String role;
--- src/main/java/first/example/firstclass/service/CustomUserDetailsService.java
+++ src/main/java/first/example/firstclass/service/CustomUserDetailsService.java
@@ -23,12 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         UserVO userData = userDAO.findByUsername(username);
         
         if (userData == null) {
-            System.out.println(">>> DAO 결과: NULL (DB에서 사용자를 찾지 못함)");
-            System.out.println("=====================================================");
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username);
-        } else {
-            System.out.println(">>> DAO 결과: " + userData.toString());
-            System.out.println("=====================================================");
         }
 
         return new CustomUserDetails(userData);
